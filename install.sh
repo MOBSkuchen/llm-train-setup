9f5fc7bda23e2765aeecd480b38a0a7126f8952f
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2023-07-26 14:59:09 +0200

    Added files

---
+++ install.sh
@@ -0,0 +1,16 @@
+echo 'Installing python'
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.10
+sudo apt-get install python3.10-distutils
+curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
+echo 'Done'
+echo 'Extracting package'
+tar -xzvf packaged.tar.gz
+echo 'Done'
+cd h2o-llmstudio-train-copy
+echo 'Installing dependencies'
+python3 -m pip install -r requirements.txt
+echo 'Done'
+echo 'Running training with config : config.yaml'
+python3 train_model.py
+echo 'Done'
